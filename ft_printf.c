0c711398a4e15c35ec65952bcb3281ffd59cedf2
Magan Josue <majosue@vo-k1.21-school.ru>
2019-11-20 21:14:48 +0300

    varg

--- ft_printf.c
+++ ft_printf.c
@@ -6,45 +6,64 @@
 /*   By: majosue <majosue@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/13 10:34:22 by majosue           #+#    #+#             */
-/*   Updated: 2019/11/19 20:24:35 by majosue          ###   ########.fr       */
+/*   Updated: 2019/11/20 21:14:13 by majosue          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include <stdio.h>
 
-int	ft_printstr(t_list **str)
+void ft_cleanup(void *content, size_t content_size)
 {
-	int i;
+	ft_bzero(content, content_size);
+	free(content);
+}
+
+int	ft_printstr(t_list *str)
+{
+	size_t i;
 	int n;
 
 	i = 0;
 	n = 0;
-	while (*str)
+	while (str)
 	{
-		while (i < (*str)->content_size)
+		while (i < str->content_size)
 		{
-			ft_putchar(((char *)(*str)->content)[i]);
+			ft_putchar(((char*)str->content)[i]);
 			i++;
 			n++;
 		}
 		i = 0;
-		*str = (*str)->next;
+		str = str->next;
 	}
 	return (n);
 }
+int ft_format(t_list **str, t_list *begin, va_list ap)
+{
+	begin = *str;
+	ft_printstr(begin);
 
+return (1);
+}
 int		ft_printf(const char *restrict format, ...)
 {
 	t_list *str;
+	va_list ap;
+	void (*del)(void*, size_t);
 
+	del = &ft_cleanup;
 	str = 0;
 	if (ft_readformat(&str, (char *)format) == -1) //получили спискок строк вида строка строка формат строка и т.д.
 		{
-			//ft_claenup(&str);
+			ft_lstdel(&str,del);
 			return (0);
 		}
-	ft_printstr(&str);
+	va_start(ap, format);
+	ft_format(&str, 0, ap);
+	va_end(ap);
+	ft_printstr(str);
+	ft_lstdel(&str,del);
 	return (100);
 }
 
@@ -54,7 +73,7 @@ int		main()
 
 	tmp = 126;
 	//  ft_endc();
-	ft_printf("well worked %lli");
+	ft_printf("well worked %lli", 10);
 
 	//printf("Проверка работы параметров%------R----------------25.25hhd\n", tmp);
 
